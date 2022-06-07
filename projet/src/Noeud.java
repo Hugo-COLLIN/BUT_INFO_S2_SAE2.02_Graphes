a0ea67ecf25a83680baca475866b349f314218a5
Hugo COLLIN <hugo.collin1@outlook.fr>
2022-06-07 09:23:56 +0200

    javadoc

--- projet/src/Noeud.java
+++ projet/src/Noeud.java
@@ -4,24 +4,50 @@ import java.util.Objects;
 
 public class Noeud
 {
+    /**
+     * nom du nœud qui permet de l’identifier
+     */
     String nom;
+
+    /**
+     *  liste des arcs reliant ce nœud `a ses nœuds adjacents
+     */
     List<Arc> adj;
 
+    /**
+     * constructeur prenant en param`etre le nom du noeud
+     * et initialisant la liste adj `a une liste vide
+     * @param nomP nom du noeud
+     */
     public Noeud (String nomP)
     {
         this.nom = nomP;
         this.adj = new ArrayList<Arc>();
     }
 
+    /**
+     * redefinition de la methode
+     * equals(Object o) dans la classe Noeud qui specifie que deux noeuds sont egaux
+     * si et seulement si leurs noms sont egaux (on suppose ainsi que le nom d’un noeud
+     * identifie le noeud de maniere unique).
+     * @param o
+     * @return boolean representant l'egalite des noeuds
+     */
     @Override
     public boolean equals(Object o) {
         Noeud p = (Noeud) o;
         return this.nom.equals(p.nom);
     }
 
-    public void ajouterArc(String dest, double cout)
+    /**
+     *  ajoute un arc allant vers le noeud destination avec un cout de cout a la liste
+     * adj.
+     * @param destination
+     * @param cout
+     */
+    public void ajouterArc(String destination, double cout)
     {
-        Noeud n = new Noeud(dest);
+        Noeud n = new Noeud(destination);
         this.adj.add(new Arc(n, cout));
     }
 
