e1779aab4cab73c7c52a8746d71ace4234d482a3
Hugo COLLIN <hugo.collin1@outlook.fr>
2022-06-07 08:52:46 +0200

    Noeud

--- projet/src/Noeud.java
+++ projet/src/Noeud.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Noeud {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Noeud
+{
+    String nom;
+    List<Arc> adj;
+
+    public Noeud (String nomP)
+    {
+        this.nom = nomP;
+        this.adj = new List<Arc>();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Noeud noeud = (Noeud) o;
+        return Objects.equals(nom, noeud.nom) && Objects.equals(adj, noeud.adj);
+    }
 }
