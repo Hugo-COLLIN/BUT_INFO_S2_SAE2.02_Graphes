87de4ca56dff861485af77da05d04541df3fcd61
FAURE Solene <solene.faure1@etu.univ-lorraine.fr>
2022-06-07 11:23:36 +0200

    Ajout de commentaires pour les methodes ajouterArc et toString

--- projet/src/GrapheListe.java
+++ projet/src/GrapheListe.java
@@ -55,16 +55,20 @@ public class GrapheListe implements Graphe {
      * @param cout cout de cet arc ajoute
      */
     public void ajouterArc(String depart, String destination, double cout) {
+        // creation et ajout des noeuds en parametre
         Noeud nD=new Noeud(depart);
         Noeud nA=new Noeud(destination);
         ensNoeuds.add(nD);
         ensNoeuds.add(nA);
         ensNom.add(depart);
         ensNom.add(destination);
+        // verification que les noeuds existent bien dans les listes
         boolean noeudTrouveDepart=false;
         boolean noeudTrouveArrive=false;
+        // indice des noeuds (iD = indice depart, iA = indice destination)
         int iD=-1;
         int iA=-1;
+        // parcours des listes pour verifier la presence des noeuds et leurs positions
         for (int i=0; i<this.ensNoeuds.size(); i++) {
             if (this.ensNoeuds.get(i).getNom().equals(depart)) {
                 noeudTrouveDepart=true;
@@ -75,6 +79,7 @@ public class GrapheListe implements Graphe {
                 iA=i;
             }
         }
+        // si les noeuds existent alors on peut ajouter l arc avec le cout en parametre
         if ((noeudTrouveArrive)&&(noeudTrouveDepart)) {
             ensNoeuds.get(iD).ajouterArc(ensNoeuds.get(iA).getNom(), cout);
         }
@@ -82,12 +87,16 @@ public class GrapheListe implements Graphe {
 
     public String toString() {
         StringBuilder msg=new StringBuilder("");
+        // La premiere boucle d indentation i s occupe des noeuds de depart
         for (int i=0; i<this.ensNoeuds.size(); i++) {
             msg.append(this.ensNoeuds.get(i).getNom()+" -> ");
+            // La deuxieme boucle d indentation j s occupe des noeuds d arrivee et du coup
+            // de l arc utilise
             for (int j=0; j<this.ensNoeuds.get(i).getAdj().size(); j++) {
                 msg.append(this.ensNoeuds.get(i).getAdj().get(j).getDest()
                 + "(" + this.ensNoeuds.get(i).getAdj().get(j).getCout() + ")");
             }
+            // saut a la ligne
             msg.append("/n");
         }
         return msg.toString();
