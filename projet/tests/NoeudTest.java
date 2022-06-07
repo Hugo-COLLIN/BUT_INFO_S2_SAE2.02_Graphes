f23a9b57e3a1bf6030b42ec88cfd2752bfeed416
Hugo COLLIN <hugo.collin1@outlook.fr>
2022-06-07 09:46:26 +0200

    tests

---
+++ projet/tests/NoeudTest.java
@@ -0,0 +1,93 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NoeudTest {
+
+    Noeud n1, n2;
+
+    @Test
+    void equals_memeNom()
+    {
+        //Initialisation
+        n1 = new Noeud("Noeud1");
+        n2 = new Noeud("Noeud1");
+
+        //Method
+        boolean res = n1.equals(n2);
+
+        //Test
+        assertTrue(res, "Noeuds doivent etre egaux car de meme nom");
+    }
+
+    @Test
+    void equals_nomDifferent()
+    {
+        //Initialisation
+        n1 = new Noeud("Noeud1");
+        n2 = new Noeud("Noeud2");
+
+        //Method
+        boolean res = n1.equals(n2);
+
+        //Test
+        assertFalse(res, "Noeuds ne doivent pas etre egaux car pas le meme nom");
+    }
+
+    @Test
+    void ajouterArc_ok()
+    {
+        //Initialisation
+        n1 = new Noeud("Noeud1");
+
+        //Method
+        n1.ajouterArc("Noeud2", 3);
+
+
+
+
+    }
+
+    @Test
+    void getNom_ok()
+    {
+        //Initialisation
+        n1 = new Noeud("Noeud1");
+
+        String res = n1.getNom();
+
+        //Test
+        assertEquals("Noeud1", res, "doit etre la meme chaine");
+
+    }
+
+    /*
+    @Test
+    void getAdj() 
+    {
+        //Initialisation
+        n1 = new Noeud("Noeud1");
+        n1.ajouterArc("Noeud2", 3);
+        List<Arc> tmp = new ArrayList<>();
+        Arc a = new Arc("Noeud2", 3)
+
+        List<Arc> res = n1.getAdj();
+
+        //Test
+        assertEquals("Noeud1", res, "doit etre la meme chaine");
+    }
+
+     */
+
+    @Test
+    void setNom() {
+    }
+
+    @Test
+    void setAdj() {
+    }
+}
