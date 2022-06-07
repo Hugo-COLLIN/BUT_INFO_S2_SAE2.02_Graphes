30c0a983585be40596eee7637d4df07e4fff8421
FAURE Solene <solene.faure1@etu.univ-lorraine.fr>
2022-06-07 09:13:50 +0200

    Ajout de l interface Graphe

--- projet/src/Arc.java
+++ projet/src/Arc.java
@@ -13,8 +13,10 @@ public class Arc {
 
     public Arc(Noeud n, double c) {
         this.dest=n.getNom();
+        // le cout doit etre strictement positif
         if (c>0) {
             this.cout=c;
+        // si le cout n est pas strictement positif, l arc n existe pas
         } else {
             this.cout=0;
         }
