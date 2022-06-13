38333cd6d15595d5687334fc957b9b5f121c7e70
Hugo COLLIN <hugo.collin1@outlook.fr>
2022-06-13 15:22:34 +0200

    BellmanFOrd

---
+++ projet/src/Main.java
@@ -0,0 +1,28 @@
+import representation.GrapheListe;
+
+public class Main
+{
+    public static void main(String[] args) {
+        GrapheListe g = new GrapheListe();
+
+        g.ajouterArc("A", "B", 12);
+        g.ajouterArc("A", "D", 87);
+        g.ajouterArc("B", "E", 11);
+        g.ajouterArc("C", "A", 19);
+        g.ajouterArc("D", "B", 23);
+        g.ajouterArc("D", "C", 10);
+        g.ajouterArc("E", "D", 43);
+
+
+        BellmanFord bF = new BellmanFord();
+        Valeur v = bF.resoudre(g, "A");
+
+        System.out.println(v);
+
+
+
+        //System.out.println(g);
+
+
+    }
+}
