1985aa5f92ab6ab971b438c848c4b79efe919c68
Hugo COLLIN <hugo.collin1@outlook.fr>
2022-06-13 11:19:11 +0200

    BellmanFord

--- projet/src/BellmanFord.java
+++ projet/src/BellmanFord.java
@@ -1,10 +1,17 @@
 import representation.Graphe;
+import representation.GrapheListe;
 
 public class BellmanFord
 {
     Valeur resoudre(Graphe g, String depart)
     {
         Valeur valeur;
+        //g = (GrapheListe)g;
+
+        for (int i = 0; i < g.listeNoeuds().size() ; i ++)
+        {
+            g.listeNoeuds().get(i).
+        }
 
         return valeur;
     }
