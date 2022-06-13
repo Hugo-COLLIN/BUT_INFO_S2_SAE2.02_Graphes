1c3b74ef5fd0f9cc679371352920168fff6ac45e
Hugo COLLIN <hugo.collin1@outlook.fr>
2022-06-13 11:31:47 +0200

    BellmanFord

--- projet/src/BellmanFord.java
+++ projet/src/BellmanFord.java
@@ -1,17 +1,37 @@
+import representation.Arc;
 import representation.Graphe;
 import representation.GrapheListe;
+
+import java.util.List;
+import java.util.Objects;
 
 public class BellmanFord
 {
     Valeur resoudre(Graphe g, String depart)
     {
-        Valeur valeur;
+        Valeur valeur = new Valeur();
         //g = (GrapheListe)g;
 
+        //init
+        valeur.setValeur(depart, 0);
         for (int i = 0; i < g.listeNoeuds().size() ; i ++)
         {
-            g.listeNoeuds().get(i).
+            if (!depart.equals(g.listeNoeuds().get(i)))
+                valeur.setValeur(g.listeNoeuds().get(i), Double.MAX_VALUE);
+            //List<Arc> lA = g.suivants(g.listeNoeuds().get(i))
         }
+
+        //etapes
+        boolean isPtFixe;
+        do {
+            isPtFixe = true;
+            for (int i = 0 ; i < g.listeNoeuds().size() ; i ++)
+            {
+
+            }
+        }
+        while (!isPtFixe);
+
 
         return valeur;
     }
