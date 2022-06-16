2bd0ef46a3b553c5ad99f228a41583fb1b37d090
Hugo COLLIN <hugo.collin1@outlook.fr>
2022-06-16 18:53:32 +0200

    MainGenLaby Fonctionnel

--- projet/src/laby/MainGenGrapheLaby.java
+++ projet/src/laby/MainGenGrapheLaby.java
@@ -6,13 +6,19 @@ import representation.GrapheListe;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+/**
+ * Q30
+ */
 public class MainGenGrapheLaby
 {
     public static void main(String[] args) throws IOException
     {
         Labyrinthe labyrinthe;
         GrapheListe g;
+        List<Valeur> valsGraphes = new ArrayList<>();
         File rep = new File("resources/labySimple/");
 
         for (File fichier : rep.listFiles())
@@ -24,7 +30,13 @@ public class MainGenGrapheLaby
 
             BellmanFord bF = new BellmanFord();
             Valeur v = bF.resoudre(g, "\"(1,1)\"");
+            valsGraphes.add(v);
 
+            //System.out.println(v.calculerChemin("\"(5,2)\""));
         }
+
+        System.out.println("\n" + valsGraphes.get(0).calculerChemin("\"(5,2)\""));
+        System.out.println(valsGraphes.get(1).calculerChemin("\"(8,5)\""));
+        System.out.println(valsGraphes.get(2).calculerChemin("\"(15,2)\""));
     }
 }
